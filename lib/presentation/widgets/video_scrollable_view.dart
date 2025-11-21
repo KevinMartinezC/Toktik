a9463449c6891dedb3bb277011b17f7112ff99b1
KevinMartinezC <ceronk003@gmail.com>
2025-11-21 10:09:43 -0600

    feat: add custom buttons

--- lib/presentation/widgets/video_scrollable_view.dart
+++ lib/presentation/widgets/video_scrollable_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/models/video_post.dart';
+import 'package:toktik/presentation/widgets/video_buttons.dart';
 
 class VideoScrollableView extends StatelessWidget {
   final List<VideoPost> videos;
@@ -7,14 +8,37 @@ class VideoScrollableView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
+    return PageView.builder(
       scrollDirection: Axis.vertical,
       physics: const BouncingScrollPhysics(),
-      children: [
-        Container(color: Colors.red),
-        Container(color: Colors.blue),
-        Container(color: Colors.teal),
-      ],
+      itemCount: videos.length,
+      itemBuilder: (context, index) {
+        final videoPost = videos[index];
+
+        return Stack(
+          children: [
+            Positioned(
+              bottom: 40,
+              right: 20,
+              child: VideoButtons(video: videoPost)
+              )
+          ],
+        );
+      },
     );
   }
 }
+
+// Doing in this way would load all the items causing performance issues 
+//  @override
+//   Widget build(BuildContext context) {
+//     return PageView(
+//       scrollDirection: Axis.vertical,
+//       physics: const BouncingScrollPhysics(),
+//       children: [
+//         Container(color: Colors.red),
+//         Container(color: Colors.blue),
+//         Container(color: Colors.teal),
+//       ],
+//     );
+//   }
